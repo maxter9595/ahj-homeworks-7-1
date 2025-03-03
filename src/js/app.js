d3d8dd866fe56a15f29a348ec17b50b13cbe0537
Maxim Terletskii <max.t95@bk.ru>
2025-03-03 21:45:15 +0300

    Use github actions

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import HelpDesk from "./HelpDesk.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const baseURL = "http://localhost:3000";
-  // const baseURL = "https://your-app.render.com/";
+  // const baseURL = "http://localhost:3000";
+  const baseURL = "https://ahj-homeworks-7-1.onrender.com";
   new HelpDesk(baseURL);
 });
